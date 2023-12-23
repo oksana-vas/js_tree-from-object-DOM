89e697f720e21a904be8e7fc74400811147a1a8b
Oksana Vasyliuk <oksana.vasyliuk13@gmail.com>
2023-12-23 15:04:54 +0100

    make build

---
+++ index.b673afc4.js
@@ -0,0 +1,2 @@
+!function e(t,n){let r=document.createElement("ul");for(let t in n){let l=document.createElement("li");l.textContent=t,r.append(l),"object"==typeof n[t]&&e(l,n[t])}t.append(r)}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.b673afc4.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,31 @@
+"use strict";
+const food = {
+    "Drink": {
+        "Wine": {},
+        "Schnaps": {}
+    },
+    "Fruit": {
+        "Red": {
+            "Cherry": {},
+            "Strawberry": {}
+        },
+        "Yellow": {
+            "Banana": {},
+            "Pineapple": {}
+        }
+    }
+};
+const tree = document.querySelector("#tree");
+function createTree(element, data) {
+    const list = document.createElement("ul");
+    for(const key in data){
+        const listItem = document.createElement("li");
+        listItem.textContent = key;
+        list.append(listItem);
+        if (typeof data[key] === "object") createTree(listItem, data[key]);
+    }
+    element.append(list);
+}
+createTree(tree, food);
+
+//# sourceMappingURL=index.f75de5e1.js.map
